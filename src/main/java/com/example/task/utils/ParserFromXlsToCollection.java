cc8441cad70e57c8cdc6266b0b3d4973e6423165
HusseinHalifaev <HusseinHalifaev@github.com>
2021-01-27 17:22:12 +0300

    Update ParserFromXlsToCollection.java

--- src/main/java/com/example/task/utils/ParserFromXlsToCollection.java
+++ src/main/java/com/example/task/utils/ParserFromXlsToCollection.java
@@ -6,7 +6,6 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
